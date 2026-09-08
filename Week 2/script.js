85d7957eb0fa13e1b5bf6b87683fef9adf6b582a
ShuvshreeBokati14 <suvshreebokati@gmail.com>
2026-09-08 08:35:19 +0545

    Addition and Subtraction

---
+++ Week 2/script.js
@@ -0,0 +1,15 @@
+
+    function sum(){
+        let number1= Number(document.getElementById("number1").value);
+        let number2= Number(document.getElementById("number2").value);
+        let result = number1 + number2;
+        document.getElementById("result").innerHTML = "The sum is: " + result;  
+    }
+
+    function subtract(){
+        let number1= Number(document.getElementById("number1").value);
+        let number2= Number(document.getElementById("number2").value);
+        let result = number1 - number2;
+        document.getElementById("result").innerHTML = "The subtraction is: " + result;  
+    }
+    
